4814c3849cf4e4dc03cde1a10b8aaafab79e2f94
Aayush Kharel <aayusshkharel2@gmail.com>
2026-08-21 23:51:18 +1000

    before todays sleep

---
+++ grocery_list.py
@@ -0,0 +1,8 @@
+
+def get_grocery_list():
+    return [
+        "Woolworths Full Cream Milk 3L",
+        "Woolworths Peanuts Unsalted 375g",
+        "Cloverdale Pure Honey Twist & Squeeze 375g",
+        "U by Kotex Ultrathin Overnight Pads Long With Wings 8 pack"
+    ]
